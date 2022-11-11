c6778f7804dd2211e6a2a885a752ae42c449ae74
LilianRage <92574996+LilianRage@users.noreply.github.com>
2022-11-10 22:17:16 -1200

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,9 @@
 #include "jeu.h"
 
 
-
-
-
 int main(){
-    Player tabPlayer[NBJOUEURMAX];
-    Cases tabPlateau[NB_CASES][NB_CASES];
     int finDuJeu = 0;
-    int nbJoueur = 0;
-    int iPosDepart = 0;
-    int jPosDepart = 0;
-    PosDepart tabPosDepart[NBJOUEURMAX];
-    while(finDuJeu == 0) {
-        nbJoueur = 0;
-        jeuPrincipale(tabPlayer, nbJoueur, &finDuJeu, tabPosDepart);
-    }
-
+    jeuPrincipale(&finDuJeu);
     return 0;
 }
 

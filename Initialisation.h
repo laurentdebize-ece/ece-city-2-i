561ce73138843da4a874295dee5666aa3a10f2a6
Virgile Fievet <virgile.fiev@edu.ece.fr>
2022-11-11 15:11:56 +0100

    ajout du fichier bitmap avec les differentes masions, le terrain vague et le fond de la map + correction (ligne qui est doublée)

--- Initialisation.h
+++ Initialisation.h
@@ -1,5 +1,4 @@
 #ifndef ECE_CITY_2_I_INITIALISATION_H
-#ifndef ECE_CITY_2_I_INITIALISATION_H
 #include "Affichage.h"
 void initialiser(Cases tabPlateau[NB_CASES][NB_CASES],Bitmap tabBit[NB_MAX_BAT]);
 

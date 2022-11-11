0ccb4a3971f54f55e9f768f8204b370a7e2d625b
LilianRage <92574996+LilianRage@users.noreply.github.com>
2022-11-10 22:24:23 -1200

    Update Initialisation.h

--- Initialisation.h
+++ Initialisation.h
@@ -1,3 +1,6 @@
-
+#ifndef ECE_CITY_2_I_INITIALISATION_H
+#ifndef ECE_CITY_2_I_INITIALISATION_H
 #include "Affichage.h"
 void initialiser(Cases tabPlateau[NB_CASES][NB_CASES],Bitmap tabBit[NB_MAX_BAT]);
+
+#endif

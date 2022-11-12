2c2f273e143cb468b81d8903ad6370d8f8d14438
maeldepreville <92508089+maeldepreville@users.noreply.github.com>
2022-11-12 16:37:22 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,3 +7,8 @@ int main(){
     return 0;
 }
 
+//le graphe est une liste d'adjascence : donc chaque sommet est repéré par graphe->pSommet[numero du sommet]
+//numero du sommet est trouvé par l'indice : (i*NBCOLONNES + j)      avec i l'indice de la ligne dans notre grille et j l'indice de la colonne
+//chaque case de notre matrice est un sommet 
+//un sommet est relié seulement si l'une des cases collées a lui est une route : dans ce cas on créé une arete entre les deux
+//toutes les routes qui sont collées sont reliées dans notre graphe

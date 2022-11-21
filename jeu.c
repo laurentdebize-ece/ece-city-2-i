b409baf816cff68b3f882e20fb76cbfcdf4654e5
LilianRage <92574996+LilianRage@users.noreply.github.com>
2022-11-20 23:18:39 -1200

    Update jeu.c

--- jeu.c
+++ jeu.c
@@ -1,12 +1,19 @@
 #include "jeu.h"
 
 
+int TrouverNumeroCase(int iCase, int jCase){
+    int numeroCase = iCase + jCase*NB_CASES;
+    return numeroCase;
+}
+
 
 void jeuPrincipale(int* finDuJeu) {
 
     Cases tabPlateau[NB_CASES][NB_CASES];
     Bitmap tabBit[NB_CASES];
+    Construction tab_constru[LIGNES*COLONNES];
     Ligne ligne;
+    Graphe* graphe = creerGraphe(LIGNES*COLONNES);
 
     int comptbitmap1 = 0;
     int valeurSourisX =0;
@@ -18,6 +25,11 @@ void jeuPrincipale(int* finDuJeu) {
     int sourisDessusX = 0;
     int sourisDessusY = 0;
     int eceFloos = 500000;
+    int i_flottant = 0;
+    int j_flottant = 0;
+    int i_constant = 0;
+    int j_constant = 0;
+    int nb_element_terrain = 0;
 
 
     bool clickCarreBleu = FALSE;
@@ -69,23 +81,7 @@ void jeuPrincipale(int* finDuJeu) {
     ALLEGRO_FONT* police = NULL;
     ALLEGRO_FONT* policePetite = NULL;
 
-    ///Mael
-    bool fin_page_accueil = false, fin_choix_mode = true, curseur_sur_communiste = false, curseur_sur_capitaliste = false;
-    bool case_occupee = false, clique = false, choix_route = false, choix_maison = false, choix_centrale = false;
-    int place_souris_x = 0, place_souris_y = 0;
-    int i_flottant = 0, j_flottant = 0, i_constant = 0, j_constant = 0;
-    int nb_element_terrain = 0, timer_secondes = 0, timer_minutes = 0;
-    bool timer_2 = false;
-
-    int* curseur_x = &place_souris_x;
-    int* curseur_y = &place_souris_y;
-    int compteur = 0;
-    int compteur2 = 0;
-    int ajoutX = 0;
-    int ajoutY = 0;
-
     /*
-    Construction tab_constru[LIGNES*COLONNES];
 
     Repere repere_grille;
 
@@ -105,8 +101,8 @@ void jeuPrincipale(int* finDuJeu) {
 
     init_mairie(&mairie);
 
-    */
-    ALLEGRO_TIMER* temps_cycle = NULL;
+
+    ALLEGRO_TIMER* temps_cycle = NULL;*/
 
 
 
@@ -132,14 +128,14 @@ void jeuPrincipale(int* finDuJeu) {
     queue = al_create_event_queue();
 
     /// valeur timer
-    timer = al_create_timer(0.010);
+    timer = al_create_timer(0.05);
     timer1sec = al_create_timer(1);
-    temps_cycle = al_create_timer(1.0);
+    //temps_cycle = al_create_timer(1.0);
 
     /// Chargement bitmpa
     TerrainVague = al_load_bitmap("../bitmap/terrainvague(true).png");
-    Maison1 = al_load_bitmap("../bitmap/house1(bg).png");
-    Maison2 = al_load_bitmap("../bitmap/house2(bg).png");
+    Maison1 = al_load_bitmap("../bitmap/house1(bg)2.png");
+    Maison2 = al_load_bitmap("../bitmap/house2(bg)2.png");
     Maison3 = al_load_bitmap("../bitmap/house3(bg).png");
     Maison4 = al_load_bitmap("../bitmap/house4(bg).png");
     MapFond = al_load_bitmap("../bitmap/mapfinie.png");
@@ -185,7 +181,7 @@ void jeuPrincipale(int* finDuJeu) {
     // Premier affichage
     al_register_event_source(queue, al_get_display_event_source(display));
     al_register_event_source(queue, al_get_timer_event_source(timer));
-    al_register_event_source(queue, al_get_timer_event_source(temps_cycle));
+    //al_register_event_source(queue, al_get_timer_event_source(temps_cycle));
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_mouse_event_source());
 
@@ -219,8 +215,23 @@ void jeuPrincipale(int* finDuJeu) {
                                     if(choixCarreRoute == TRUE ) {
                                         tabPlateau[i][j].occupe = 1;
                                         tabPlateau[i][j].routeOccupe = 1;
+                                        nb_element_terrain++;
                                         eceFloos = eceFloos - 10;
                                         tabPlateau[i][j].valeurCompteurAuClic = al_get_timer_count(timer);
+                                        if(i+1 < 45) {
+                                            tabPlateau[i + 1][j].viable = 1;
+                                        }
+                                        if(i-1 > 0) {
+                                            tabPlateau[i - 1][j].viable = 1;
+                                        }
+                                        if(j+1 < 45) {
+                                            tabPlateau[i][j + 1].viable = 1;
+                                        }
+                                        if(j-1 > 0) {
+                                            tabPlateau[i][j - 1].viable = 1;
+                                        }
+                                        tabPlateau[i][j].viable = 1;
+                                        //CreerArete(graphe->pSommet, i,j,i,j,TrouverNumeroCase(i, j), );
                                     }
                                     if(clickCarreBleu == TRUE){
                                         //tabBit[comptbitmap1].batPose = 1;
@@ -228,7 +239,7 @@ void jeuPrincipale(int* finDuJeu) {
                                         //tabBit[comptbitmap1].j = j;
                                         for (int k = i; k <= i+NB_Cases_Place_ConstruX; ++k) {
                                             for (int l = j-NB_Cases_Place_ConstruY; l <= j; ++l) {
-                                                if(tabPlateau[k][l].occupe == 1){
+                                                if(tabPlateau[k][l].occupe == 1 || j-NB_Cases_Place_ConstruY < 0 || i+NB_Cases_Place_ConstruX > 44){
                                                     impossiblePlacerBat = TRUE;
                                                     tabBit[comptbitmap1].batPose = 0;
                                                 }
@@ -237,16 +248,19 @@ void jeuPrincipale(int* finDuJeu) {
                                         if(impossiblePlacerBat == FALSE){
                                             for (int k = i; k <= i+NB_Cases_Place_ConstruX; ++k) {
                                                 for (int l = j - NB_Cases_Place_ConstruY; l <= j; ++l) {
-                                                    tabPlateau[k][l].occupe = 1;
-                                                    tabBit[comptbitmap1].batPose = 1;
-                                                    tabBit[comptbitmap1].empecherDeRentrerPlusieursFoisDAnsLaBoucle = 0;
-                                                    tabBit[comptbitmap1].valeurCompteurAuClic = al_get_timer_count(timer);
-                                                    tabBit[comptbitmap1].i = i;
-                                                    tabBit[comptbitmap1].j = j;
-                                                    tabBit[comptbitmap1].chance1sur4 = rand()%3;
-                                                    tabBit[comptbitmap1].changemetnBat = 0;
+                                                        tabPlateau[k][l].occupe = 1;
+                                                        tabBit[comptbitmap1].batPose = 1;
+                                                        tabBit[comptbitmap1].empecherDeRentrerPlusieursFoisDAnsLaBoucle = 0;
+                                                        tabBit[comptbitmap1].valeurCompteurAuClic = al_get_timer_count(
+                                                                timer);
+                                                        tabBit[comptbitmap1].i = i;
+                                                        tabBit[comptbitmap1].j = j;
+                                                        tabBit[comptbitmap1].chance1sur4 = rand() % 3;
+                                                        tabBit[comptbitmap1].changemetnBat = 0;
+
                                                 }
                                             }
+                                            nb_element_terrain++;
                                             nbNombreBatPose++;
                                             comptbitmap1++;
                                             eceFloos = eceFloos - 1000;
@@ -257,6 +271,22 @@ void jeuPrincipale(int* finDuJeu) {
                                 }else{
                                     if(EnleverRoute == TRUE){
                                         tabPlateau[i][j].occupe = 0;
+                                        if(tabPlateau[i+1][j].routeOccupe == 0) {
+                                            tabPlateau[i + 1][j].viable = 0;
+                                        }
+                                        if(tabPlateau[i-1][j].routeOccupe == 0) {
+                                            tabPlateau[i-1][j].viable = 0;
+
+                                        }
+                                        if(tabPlateau[i][j+1].routeOccupe == 0){
+                                            tabPlateau[i][j+1].viable = 0;
+
+                                        }
+                                        if(tabPlateau[i][j-1].routeOccupe == 0){
+                                            tabPlateau[i][j-1].viable = 0;
+                                        }
+                                        tabPlateau[i][j].viable = 0;
+                                        nb_element_terrain--;
                                         EnleverRoute = FALSE;
                                     }
                                 }
@@ -301,8 +331,119 @@ void jeuPrincipale(int* finDuJeu) {
                             }
                         }
                     }
+
+
+                    ///Mael
+                    i_constant = i_flottant;
+                    j_constant = j_flottant;
+
+                    /*if (choixCarreRoute == TRUE) {
+                        if (tabPlateau[i_constant][j_constant].occupe == 0) {
+
+                            tabPlateau[i_constant][j_constant].occupe = 1;
+                            tabPlateau[i_constant][j_constant].type = 0;
+                            tabPlateau[i_constant][j_constant].id_bat_pose = i_constant * COLONNES + j_constant;
+
+                            tab_constru[i_constant * COLONNES + j_constant].pos_x = tabPlateau[i_constant][j_constant].x1;
+                            tab_constru[i_constant * COLONNES + j_constant].pos_y = tabPlateau[i_constant][j_constant].y1;
+                            tab_constru[i_constant * COLONNES + j_constant].type = 0;
+                            tab_constru[i_constant * COLONNES + j_constant].nb_cases_largeur = tab_constru[nb_element_terrain].nb_cases_hauteur = 1;
+
+
+                            check_relier_cases(graphe, i_constant, j_constant, 0, 0, i_constant - 1, j_constant, true, tab_constru, tabPlateau);
+                            check_relier_cases(graphe, i_constant, j_constant, 0, 0, i_constant + 1, j_constant, true, tab_constru, tabPlateau);
+                            check_relier_cases(graphe, i_constant, j_constant, 0, 0, i_constant, j_constant - 1, true, tab_constru, tabPlateau);
+                            check_relier_cases(graphe, i_constant, j_constant, 0, 0, i_constant, j_constant + 1, true, tab_constru, tabPlateau);
+
+
+                        }
+                    }*/
+
+                    /*if (clickCarreBleu == TRUE) {
+                        if (tabPlateau[i_constant][j_constant].occupe == 0 && tabPlateau[i_constant - 1][j_constant - 1].occupe == 0 && tabPlateau[i_constant + 1][j_constant + 1].occupe == 0
+                            && tabPlateau[i_constant - 1][j_constant].occupe == 0 && tabPlateau[i_constant + 1][j_constant].occupe == 0 && tabPlateau[i_constant][j_constant - 1].occupe == 0
+                            && tabPlateau[i_constant][j_constant + 1].occupe == 0 && tabPlateau[i_constant - 1][j_constant + 1].occupe == 0 && tabPlateau[i_constant + 1][j_constant - 1].occupe == 0) {
+
+
+                            for (int i = -1; i < 2; i++) {
+                                for (int j = -1; j < 2; j++) {
+                                    tabPlateau[i_constant + i][j_constant + j].occupe = 1;
+                                    tabPlateau[i_constant + i][j_constant + j].id_bat_pose = i_constant * COLONNES + j_constant;
+
+                                    check_relier_cases(graphe, i_constant, j_constant, i, j, i_constant - 1, j_constant,false, tab_constru, tabPlateau);
+                                    check_relier_cases(graphe, i_constant, j_constant, i, j, i_constant + 1, j_constant,false, tab_constru, tabPlateau);
+                                    check_relier_cases(graphe, i_constant, j_constant, i, j, i_constant, j_constant - 1,false, tab_constru, tabPlateau);
+                                    check_relier_cases(graphe, i_constant, j_constant, i, j, i_constant, j_constant + 1,false, tab_constru, tabPlateau);
+
+                                }
+                            }
+
+
+                            tab_constru[i_constant * COLONNES + j_constant].pos_x = tabPlateau[i_constant][j_constant].x1;
+                            tab_constru[i_constant * COLONNES + j_constant].pos_y = tabPlateau[i_constant][j_constant].y1;
+                            tab_constru[i_constant * COLONNES + j_constant].type = 1;
+                            tab_constru[i_constant * COLONNES + j_constant].nb_cases_largeur = tab_constru[nb_element_terrain].nb_cases_hauteur = 3;
+
+
+                        }
+                    }*/
+
+                    /*if (choix_centrale) {
+                        if (tabPlateau[i_constant - 2][j_constant - 3].occupee == 0 && tabPlateau[i_constant - 2][j_constant - 2].occupee == 0 && tabPlateau[i_constant - 2][j_constant - 1].occupee == 0
+                            && tabPlateau[i_constant - 2][j_constant].occupee == 0 && tabPlateau[i_constant - 2][j_constant + 1].occupee == 0 && tabPlateau[i_constant - 2][j_constant + 2].occupee == 0
+                            && tabPlateau[i_constant - 1][j_constant - 3].occupee == 0 && tabPlateau[i_constant - 1][j_constant - 2].occupee == 0 && tabPlateau[i_constant - 1][j_constant - 1].occupee == 0
+                            && tabPlateau[i_constant - 1][j_constant].occupee == 0 && tabPlateau[i_constant - 1][j_constant + 1].occupee == 0 && tabPlateau[i_constant - 1][j_constant + 2].occupee == 0
+                            && tabPlateau[i_constant][j_constant - 3].occupee == 0 && tabPlateau[i_constant][j_constant - 2].occupee == 0 && tabPlateau[i_constant][j_constant - 1].occupee == 0
+                            && tabPlateau[i_constant][j_constant].occupee == 0 && tabPlateau[i_constant][j_constant + 1].occupee == 0 && tabPlateau[i_constant][j_constant + 2].occupee == 0
+                            && tabPlateau[i_constant + 1][j_constant - 3].occupee == 0 && tabPlateau[i_constant + 1][j_constant - 2].occupee == 0 && tabPlateau[i_constant + 1][j_constant - 1].occupee == 0
+                            && tabPlateau[i_constant + 1][j_constant].occupee == 0 && tabPlateau[i_constant + 1][j_constant + 1].occupee == 0 && tabPlateau[i_constant + 1][j_constant + 2].occupee == 0
+                            && mairie.monnaie >= 100000) {
+
+                            clique = true;
+
+                            for (int i = -2; i < 2; i++) {
+                                for (int j = -3; j < 3; j++) {
+
+                                    tabPlateau[i_constant + i][j_constant + j].occupee = 1;
+                                    tabPlateau[i_constant + i][j_constant + j].id_bat_pose =
+                                            i_constant * COLONNES + j_constant;
+                                    check_relier_cases(graphe, i_constant, j_constant, i, j, i_constant + i - 1,j_constant + j, false, tab_constru);
+                                    check_relier_cases(graphe, i_constant, j_constant, i, j, i_constant + i + 1,j_constant + j, false, tab_constru);
+                                    check_relier_cases(graphe, i_constant, j_constant, i, j, i_constant + i,j_constant + j - 1, false, tab_constru);
+                                    check_relier_cases(graphe, i_constant, j_constant, i, j, i_constant + i,j_constant + j + 1, false, tab_constru);
+
+                                }
+                            }
+
+
+                            tab_constru[i_constant * COLONNES + j_constant].pos_x = tabPlateau[i_constant][j_constant].x;
+                            tab_constru[i_constant * COLONNES + j_constant].pos_y = tabPlateau[i_constant][j_constant].y;
+                            tab_constru[i_constant * COLONNES + j_constant].type = 2;
+                            tab_constru[i_constant * COLONNES + j_constant].nb_cases_largeur = 6;
+                            tab_constru[i_constant * COLONNES + j_constant].nb_cases_hauteur = 4;
+
+                            mairie.monnaie -= 100000;
+
+                        }
+
+                    }*/
+
+                    /*for(int a = 0; a < LIGNES; a++) {
+                        for(int b = 0; b < COLONNES; b++) {
+                            if(tab_constru[a*COLONNES + b].viable) {
+                                for(int c = 0; c < LIGNES; c++) {
+                                    for(int d = 0; d < COLONNES; d++) {
+                                        if(tabPlateau[c][d].id_bat_pose == tabPlateau[a][b].id_bat_pose) {
+                                            tab_constru[c*COLONNES + d].viable = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }*/
+
                 }
-                
+
 
                 break;
 
@@ -323,31 +464,27 @@ void jeuPrincipale(int* finDuJeu) {
                 break;
             }
 
-            case ALLEGRO_EVENT_TIMER:
-                if(event.timer.source == timer) {
+
+            case ALLEGRO_EVENT_TIMER: {
+                if (event.timer.source == timer) {
+                    //printf("%d\n", nb_element_terrain);
                     if (al_get_timer_count(timer1sec) == 60) {
                         al_stop_timer(timer1sec);
                         al_set_timer_count(timer1sec, 0);
                         al_start_timer(timer1sec);
                         minutes++;
                     }
-
-                    dessinerLeJeu(tabPlateau, tabBit, sourisDessusX, sourisDessusY, ligne, nbNombreBatPose, valeurSourisX, valeurSourisY, minutes,&nbHabTotal,&eceFloos, clickCarreBleu, affichageRoute, affichageEau, affichageElec, timer, timer1sec, police,policePetite, RouteDroite, RouteDroite2, Tournant, Tournant2, Tournant3, Tournant4, DoubleTournant, DoubleTournant2, DoubleTournant3, DoubleTournant4, Croisement, TerrainVague, Maison1, Maison2, Maison3, Maison4, MapFond, NuageFond, Ruine, Immeuble, GratteCiel, Cabane, PieceMonnaie, Habitant);
-
-
-
-                    al_flip_display();
+                    dessinerLeJeu(tabPlateau, tabBit, sourisDessusX, sourisDessusY, ligne, nbNombreBatPose,valeurSourisX, valeurSourisY, minutes, &nbHabTotal, &eceFloos, clickCarreBleu,affichageRoute, affichageEau, affichageElec, timer, timer1sec, police, policePetite,RouteDroite, RouteDroite2, Tournant, Tournant2, Tournant3, Tournant4, DoubleTournant,DoubleTournant2, DoubleTournant3, DoubleTournant4, Croisement, TerrainVague, Maison1,Maison2, Maison3, Maison4, MapFond, NuageFond, Ruine, Immeuble, GratteCiel, Cabane,PieceMonnaie, Habitant);
+                    for (int i=0; i<nb_element_terrain; i++) {
+                        while (graphe->pSommet[i]->arc != NULL){
+                            printf("%d\n",graphe->pSommet[i]->arc->i_sommet);
+                            printf("%d\n\n",graphe->pSommet[i]->arc->j_sommet);
+                            graphe->pSommet[i]->arc=graphe->pSommet[i]->arc->arc_suivant;
+                        }
+                    }
                 }
-
-
-
-
-
-
-
-
-
                 break;
+            }
         }
 
     }

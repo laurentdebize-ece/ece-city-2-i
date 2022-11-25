eb2661b90704e3dd839bb0e7bfc5fe6e33d1a930
LilianRage <92574996+LilianRage@users.noreply.github.com>
2022-11-25 05:56:35 -1200

    Update Graphe.h

--- Graphe.h
+++ Graphe.h
@@ -1,5 +1,6 @@
 #ifndef ECE_CITY_2_I_GRAPHE_H
 #define ECE_CITY_2_I_GRAPHE_H
+
 #include <stdio.h>
 #include <stdlib.h>
 //#include "map.h"
@@ -112,6 +113,7 @@ typedef struct Som{
     int typeUsineEau;
     int typeUsineElec;
     int sommetPrecedent;
+    int NumeroDelaRoutePose;
     int numeroSommet;
     int numeroCompoConnexe;
     int numeroCaseSommetAdj[4];
@@ -124,12 +126,16 @@ typedef struct {
 
 Graphe* creerGraphe(int ordre);
 void ajouterCompoConnexe(Sommet2** sommet, Cases tabPlateau[COLONNES][COLONNES], int i, int j);
-void Parcourir(Sommet2 * m, Cases tabPlateau[COLONNES][COLONNES], int i, int j,int compteurUsineEau, int compteurUsineElec);
+void Parcourir(Sommet2 * m, Cases tabPlateau[COLONNES][COLONNES], int i, int j,int compteurUsineEau, int compteurUsineElec, int numConnexeUsineEAu, int numConnexeUsineElec);
 void SupprimerArete(Sommet2** sommet,Cases tabPlateau[COLONNES][COLONNES], int i, int j);
 void afficher(Sommet2 * m, Cases tabPlateau[COLONNES][COLONNES], int i, int j);
-void CreerArete2(Sommet2 ** Psommet2, int i, int j, int i_bis, int j_bis, int numeroCase, int PremierSommet, int tabMarque[45*35], Cases tabPlateau[COLONNES][COLONNES], int* sommetPrec);
+void CreerArete2(Sommet2 ** Psommet2, int i, int j, int i_bis, int j_bis, int numeroCase, int PremierSommet, int tabMarque[45*35], Cases tabPlateau[COLONNES][COLONNES], int* sommetPrec, int nbRoutesPose);
 pSommet* CreerArete(pSommet* sommet, int i, int j, int i_bis, int j_bis, int numeroCase,  int premierSommet, int tabMarque[45*35]);
 Sommet2 * Trier(Sommet2 * m);
+Sommet2 * TrierDansLeBonSensDeLaPose(Sommet2 * m);
 //int TrouverNumeroCase(int iCase, int jCase);
 void check_relier_cases(Graphe* g, int i_constant, int j_constant, int i , int j, int i_bis, int j_bis, bool pose_de_route, Construction* tab_constru, Cases tab[COLONNES][COLONNES]);
+
+
+
 #endif

f4e619d67ab1ade4054617d8dd5b11126bf733b1
LilianRage <92574996+LilianRage@users.noreply.github.com>
2022-11-26 09:59:22 -1200

    Update jeu.h

--- jeu.h
+++ jeu.h
@@ -8,5 +8,10 @@
 
 void jeuPrincipale(int* finDuJeu);
 int TrouverNumeroCase(int iCase, int jCase);
+void ComposanteConnexeDuGraphe(Cases tabPlateau[NB_CASES][NB_CASES], Sommet2* Psommet2,bool compteurUsineEau,bool compteurUsineElec, int numeroConneDeUsineEau, int numeroConnexeDeUsineElec);
+void SourisSurCase(Cases tabPlateau[NB_CASES][NB_CASES], int* valeurSourisX, int* valeurSourisY, int* sourisDessusX, int* sourisDessusY, ALLEGRO_EVENT event);
+void RegarderCaseAutour(Cases tabPlateau[NB_CASES][NB_CASES], Bitmap tabBit[NB_MAX_BAT], int comptbitmap1, int* nbBatAutourDesRoutes, ALLEGRO_TIMER* timer, int i, int j);
+void choixModes(ALLEGRO_EVENT event, bool* clickCarreBleu, bool* choixCarreRoute, bool* affichageElec, bool* affichageEau, bool* affichageRoute, bool* choixUsineEau, bool* choixUsineElec, ALLEGRO_FONT* police);
+
 
 #endif

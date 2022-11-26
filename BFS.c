24a8269e93801b96ce9205b143a1969f299b9738
Virgile Fievet <virgile.fiev@edu.ece.fr>
2022-11-26 17:04:11 +0100

    BFS fini (normalement) manque le tri des distances

--- BFS.c
+++ BFS.c
@@ -1,51 +1,12 @@
 #include "BFS.h"
 #include "Graphe.h"
 
-void enfiler(t_file *f,int num){
-    t_maillon* tmp= malloc(sizeof(t_maillon));
-    tmp->num=num;
-    tmp->suiv=NULL;
-
-    if(f->tete==NULL){
-        f->tete=tmp;
-        f->fin=tmp;
-    }
-    else{
-        f->fin->suiv=tmp;
-        f->fin=tmp;
-
-    }
-
-}
-
-int defiler(t_file *f){
-
-    t_maillon* tmp=f->tete;
-    if(f->tete == f->fin){
-        f->fin=NULL;
-        f->tete=NULL;
-    }
-    else {
-        f->tete = f->tete->suiv;
-    }
-    int num= tmp->num;
-    free(tmp);
-
-    return num;
-}
-
-
-
-
-
 
 
 int BFS(Sommet2 ** Psommet2, int nbbathab){ // prend en parametre le graphe (sous forme de liste d'adjacence) et le sommet initial
 
     int tabDistanceChateau[10][15];
     //premier indice : numéro chateau ,2e indice : numéro maison
-
-    bool end = false;
 
     //On met la distance de tous les sommet habitation à 0 :
     Sommet2* temp1 = *Psommet2;
@@ -57,37 +18,30 @@ int BFS(Sommet2 ** Psommet2, int nbbathab){ // prend en parametre le graphe (sou
     }
 
 
-
     for(int j=0; j <nbbathab; j++){ //effectue pour chaque habitation le calcul de distance
         Sommet2* temp2 = *Psommet2;
-        while(!end){
-            while (temp2 != NULL) {
-                while(temp2->typeUsineEau != 1){
-                    if(temp2->marque !=1){
-                        temp2->marque = 1;
-                        (*Psommet2)->distance++;
-                        temp2 = temp2->next;
-                    }
-                    end = true;
+        while (temp2 != NULL && temp2->marque !=1) {
+            //on vérifie que l'on peut aller au sommet suivant
 
-                    //à la sortie ajouter l'association de la distance au chateau en question
+            while(temp2->typeUsineEau != 1){
+                //on passe au sommet suivant tant que le sommet actuel n'est pas un chateau d'eau
 
-
-                }
-                //trouver comment obtenir le numéro du chateau pour tabDistanceChateau[10][j];
-                tabDistanceChateau[(temp2)->numeroChateau][j] = (*Psommet2)->distance;
-
+                temp2->marque = 1;
+                (*Psommet2)->distance++;
+                temp2 = temp2->next;
 
             }
+            //si le sommet actuel est un chateau d'eau, on note la distance dans le tableau qui recense toutes les distances de
+            tabDistanceChateau[(temp2)->numeroChateau][j] = (*Psommet2)->distance;
+
         }
-        //reset des marques
+        //reset des marques avant de passer au sommet habitation suivant
         while(temp1->next->marque != 0){
             temp1->marque = 0;
             temp1 = temp1->next;
         }
 
-
     }
-
+    //faire une fonction qui trie les distances
     return 0;
 }
